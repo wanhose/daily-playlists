87d27e6bfc39983ebdaa363106f63cab80000b20
wanhose <hello@wanhose.dev>
2024-02-29 02:29:29 +0100

    feat: implement input change query search params

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,29 @@
-export const Input = () => (
-  <input
-    className="mt-4 w-full rounded-md border border-gray-700 bg-black px-4 py-2 text-xl text-white focus:border-spotify focus:outline-none"
-    type="text"
-    placeholder="Search"
-  />
-);
+import { ChangeEvent, useCallback } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+export const Input = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchValue = searchParams.get('s') || '';
+
+  const handleChange = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      if (event.currentTarget.value) {
+        setSearchParams({ s: event.currentTarget.value });
+      } else {
+        searchParams.delete('s');
+        setSearchParams(searchParams);
+      }
+    },
+    [searchParams, setSearchParams]
+  );
+
+  return (
+    <input
+      className="mt-4 w-full rounded-md border border-gray-700 bg-black px-4 py-2 text-xl text-white focus:border-spotify focus:outline-none"
+      onChange={handleChange}
+      placeholder="Search"
+      type="text"
+      value={searchValue}
+    />
+  );
+};
