86faf6375b7cf07139ebd5cbe16a780105ac43ed
wanhose <hello@wanhose.dev>
2024-02-29 13:29:19 +0100

    fix: warning when running tests

--- src/tests/mocks/react-router-dom.ts
+++
@@ -1,7 +0,0 @@
-import { vi } from 'vitest';
-
-vi.spyOn(console, 'error').mockImplementation((...args) => {
-  if (!args[0].includes('Warning: An update to %s inside a test was not wrapped in act')) {
-    console.error(...args);
-  }
-});
--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom/vitest';
 import 'tests/mocks/@tanstack/react-query';
-import 'tests/mocks/react-router-dom';
 
 import { cleanup } from '@testing-library/react';
 import { afterEach } from 'vitest';
