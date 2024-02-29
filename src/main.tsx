e4549b6d9e4f6e03400eff6f136755b88b695c02
wanhose <hello@wanhose.dev>
2024-02-29 02:12:56 +0100

    feat: implement minimal router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
 import App from 'containers/App';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, Navigate, RouteObject, RouterProvider } from 'react-router-dom';
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '*',
+    element: <Navigate replace to="/" />,
+  },
+];
 
 const node = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(node);
+const router = createBrowserRouter(routes);
 
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
