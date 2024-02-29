4ca12218081241b1e699c7a43d946cbc73e2c72a
wanhose <hello@wanhose.dev>
2024-02-29 02:32:32 +0100

    fix: tests

---
+++ src/tests/utils.tsx
@@ -0,0 +1,27 @@
+import { Queries, queries, render, RenderOptions } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+const customRender = <
+  Q extends Queries = typeof queries,
+  Container extends Element | DocumentFragment = HTMLElement,
+  BaseElement extends Element | DocumentFragment = Container,
+>(
+  ui: React.ReactElement,
+  options?: RenderOptions<Q, Container, BaseElement>
+) => {
+  return render(ui, {
+    wrapper: ({ children }) => (
+      <>
+        <div id="root">
+          <BrowserRouter>{children}</BrowserRouter>
+        </div>
+        <div id="portal-root" />
+      </>
+    ),
+    ...options,
+  });
+};
+
+export * from '@testing-library/react';
+
+export { customRender as render };
